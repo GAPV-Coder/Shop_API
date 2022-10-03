ee5d180e3194face4e485fb7e0a683c94eaad900
Gustavo Pereira <70466896+GAPV-Coder@users.noreply.github.com>
2022-10-02 22:50:46 -0500

    Added stripe route
    Stripe payment processing routing has been added.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 const app = express();
 
@@ -18,12 +20,14 @@ mongoose
 		console.log("Sorry, an error occurred with the DB connection!", err);
 	});
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("Backend server is running!");
